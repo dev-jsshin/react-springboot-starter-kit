46bc04f26850726a710e159a791150809dc3d7c1
dev-jssin <dev.jssin@gmail.com>
2023-11-30 23:46:21 +0900

    chore: Processing api base url environment variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const BACKEND_URL = `${process.env.REACT_APP_BACKEND_URL}`;
+  const BACKEND_PORT = `${process.env.REACT_APP_BACKEND_PORT}`;
+  const BACKEND_CONTEXT_PATH = `${process.env.REACT_APP_BACKEND_CONTEXT_PATH}`;
 
   const [bsData, setBsData] = useState([]);
   const [dbData, setDbData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://' + BACKEND_URL + '/bs').then(reponse => {
+    axios.get('http://' + BACKEND_URL + ':' + BACKEND_PORT + BACKEND_CONTEXT_PATH + '/bs').then(reponse => {
       setBsData(reponse.data);
     });
     
-    axios.get('http://' + BACKEND_URL + '/db/connection').then(reponse => {
+    axios.get('http://' + BACKEND_URL + ':' + BACKEND_PORT + BACKEND_CONTEXT_PATH +  '/db/connection').then(reponse => {
       setDbData(reponse.data.data);
     });
   }, []);
